a4475f1f4524ad37a667a0d1a512e21ab6199c02
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2026-04-03 14:28:02 +0530

    getvideos.php

--- getvideos.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "video_db");
-
-$result = $conn->query("SELECT * FROM videos LIMIT 1");
-$row = $result->fetch_assoc();
-
-echo json_encode($row);
-?>
