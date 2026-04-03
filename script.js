419707d110fd650d47108bdb7c96c72a8f4fa124
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2026-04-03 14:27:31 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function loadVideo() {
-  const id = document.getElementById("videoId").value;
-  const player = document.getElementById("ytPlayer");
-
-  player.src = "https://www.youtube.com/embed/" + id;
+function loadFromDB() {
+  fetch("GetVideo")
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("ytPlayer").src =
+        "https://www.youtube.com/embed/" + data.id;
+    });
 }
