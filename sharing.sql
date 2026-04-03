95f6d844d24bc99ac7e00a54cd07774ea6ca4b13
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2026-04-03 14:00:21 +0530

    sharing.sql

--- sharing.sql
+++ sharing.sql
@@ -1,23 +1,5 @@
-CREATE TABLE Contacts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE,
-    email TEXT,
-    isBorrower BOOLEAN
-);
-
-CREATE TABLE Items (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    status TEXT,
-    borrower_id INTEGER,
-    FOREIGN KEY (borrower_id) REFERENCES Contacts(id)
-);
-
-CREATE TABLE Dimensions (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    item_id INTEGER,
-    width TEXT,
-    height TEXT,
-    length TEXT,
-    FOREIGN KEY (item_id) REFERENCES Items(id)
+CREATE TABLE videos (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(100),
+  video_url VARCHAR(255)
 );
