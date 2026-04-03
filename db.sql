01a8bdad4f4e1aea577324c7619b54ccd30be477
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2026-04-03 15:07:46 +0530

    db.sql

--- db.sql
+++ db.sql
@@ -1,8 +1,12 @@
-CREATE TABLE youtube_videos (
+CREATE DATABASE youtube_db;
+
+USE youtube_db;
+
+CREATE TABLE videos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(100),
-  video_id VARCHAR(50)
+  youtube_id VARCHAR(50)
 );
 
-INSERT INTO youtube_videos (title, video_id)
-VALUES ('My Video', 'TCE-4UzrEGw');
+INSERT INTO videos (title, youtube_id)
+VALUES ('Demo Video', 'dQw4w9WgXcQ');
